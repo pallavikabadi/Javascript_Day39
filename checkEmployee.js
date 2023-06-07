4470d3503461fc44946b9793894a35deee77aed1
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-06-07 23:01:09 +0530

    Added:Create Javascript and HTML Fles

---
+++ checkEmployee.js
@@ -0,0 +1,10 @@
+const is_present =1;
+
+let empCheck = Math.floor(Math.random()*10)%2;
+
+if(empCheck==is_present){
+    console.log("Employee is present");
+}
+else{
+    console.log("Employee is absent");
+}
